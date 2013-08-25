4bb873f95e3d17cdb754ce87aa0f1b4092c2d733
Macroon70 <peter7012@icloud.com>
2013-08-25 14:39:07 +0200

    before revert

--- js/site.js
+++ js/site.js
@@ -24,26 +24,6 @@ $('link[rel=icon]').attr('href','images/games_site/favico_'+systemColors[randomN
 	}
 
 	function sliderChanger(nextElem) {
-<<<<<<< HEAD
-		if (nextElem == 0) {
-			var actualElemPrefix = $('a.device_button.white').attr('data-filename-prefix');
-			if (++actualElemPrefix == 5) actualElemPrefix = 1;
-			nextElem = $('a[data-filename-prefix="'+actualElemPrefix + '"]');
-		}
-		// iPad refresh browser on every animate because low its memory size. Using fade-in-out effect instead
-		if (isiPad) {
-			if (!nextElem.hasClass('white') && !$(':animated').length) {
-				nextElem.addClass('white').siblings().removeClass('white');
-				var filename = 'url(images/games_site/slider' + nextElem.attr('data-filename-prefix') + '_device_bg.png)';
-				$('.inactive_main_screen')
-					.css('background-image',filename)
-					.animate({ opacity: 1}, 1000, function() {
-						$(this)
-							.removeClass('inactive_main_screen')
-							.addClass('active_main_screen');
-					});
-				$('.active_main_screen').animate({ opacity: 0}, 1000, function() {
-=======
 		var actualElemPrefix = $('div.device_button.white').attr('data-filename-prefix');
 		var nextElemPrefix;
 		if (nextElem == 0) {
@@ -60,47 +40,10 @@ $('link[rel=icon]').attr('href','images/games_site/favico_'+systemColors[randomN
 			nextElem.addClass('white').siblings().removeClass('white');
 			pushLayer
 				.fadeIn(1000, function() {
->>>>>>> v3.5
 					$(this)
 						.removeClass('active_main_screen')
 						.addClass('inactive_main_screen');
 				});
-<<<<<<< HEAD
-			}
-		} else {
-			if (!nextElem.hasClass('white') && !$(':animated').length) {
-				nextElem.addClass('white').siblings().removeClass('white');
-				var filename = 'url(images/games_site/slider' + nextElem.attr('data-filename-prefix') + '_device_bg.png)';
-				var videoPos = nextElem.attr('data-video-position').split('/')
-				$('#video_sizer').animate({
-					top : videoPos[0] + '%',
-					left: videoPos[1] + '%',
-					width : videoPos[2] + '%',
-					'transform': 'rotate('+videoPos[3]+'deg)',
-					'-webkit-transform':'rotate('+videoPos[3]+'deg)',
-					'-moz-transform':'rotate('+videoPos[3]+'deg)',
-					'-ms-transform':'rotate('+videoPos[3]+'deg)',
-					'-o-transform':'rotate('+videoPos[3]+'deg)'										
-				}, 1000);
-				$('.inactive_main_screen')
-					.css({
-						'background-image' : filename,
-						display : 'block'	})
-					.animate({ left: 0}, 1000, function() {
-						$(this)
-							.removeClass('inactive_main_screen')
-							.addClass('active_main_screen');
-					});
-				$('.active_main_screen').animate({ left: '-100%'}, 1000, function() {
-					$(this)
-						.css({ 
-							display : 'none',
-							left : '100%' })
-						.removeClass('active_main_screen')
-						.addClass('inactive_main_screen');
-				});
-			}
-=======
 			activeLayer
 				.fadeOut(1000, function() {
 					$(this)
@@ -117,7 +60,6 @@ $('link[rel=icon]').attr('href','images/games_site/favico_'+systemColors[randomN
 			$('#slider_menu_bg').animate({
 				left : (13 + (nextElemPrefix * 22)) + '%'
 			}, 1000);
->>>>>>> v3.5
 		}
 	}
 
@@ -190,13 +132,8 @@ $(document).ready(function() {
   $('#menu_close_button').css('background-image', 'url(images/games_site/button_close_'+actualSystemColor+'.png)' );
 
 
-<<<<<<< HEAD
-	$('.device_button').on({
-		click: function() {
-=======
 	$('div.device_button').on({
 		click: function(event) {
->>>>>>> v3.5
 			sliderChanger($(this));
 			clearTimeout(sliderChangerTimer);
 			sliderChangerTimer = window.setInterval(function() { sliderChanger(0); }, 15000 );			
@@ -235,12 +172,6 @@ $(document).ready(function() {
 
 	$('#menu_close_button').on({
 		click: function() {
-<<<<<<< HEAD
-			$('#menu_screen').animate({ right : '-30%'}, 500);
-		}
-	});
-
-=======
 			$('#menu_screen').animate({ right : '-130%'}, 300);
 		}
 	});
@@ -251,7 +182,6 @@ $(document).ready(function() {
 		}
 	}, true);
 
->>>>>>> v3.5
 
 	var sliderChangerTimer = window.setInterval(function() { sliderChanger(0); }, 15000 );
 
