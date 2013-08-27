9e2deddce49cacccfddaa7a08ab75eba7bff9b57
vigb <balazs.vig@datasolutions.hu>
2013-08-27 16:40:24 +0200

    Redo video bugfix

--- js/main_screen.js
+++ js/main_screen.js
@@ -162,144 +162,79 @@ $(document).ready(function() {
 
 
   // Small video players controll
-  $('.small_video_player').on({
-    click: function(event) {
-      var clickedZindex = $(this).css('z-index');
-      if (clickedZindex != 3) {
-        var idName = $(this).children().first().attr('id');
-        event.stopPropagation();
-        jwplayer( idName+'_ply').play();
-        $(this)
-          .addClass('content_border content_shadow playing_small_video')
-          .css({
-            'z-index' : 3,
-            'border-color': '#ffffff',
-            'border-width' : $(this).width() * 0.03 + 'px',
-            'border-style' : 'solid'
-          })
-          .siblings('.small_video_player').each(function() {
-            var childZindex = $(this).css('z-index');
-            if (childZindex == 3) $(this).css({'z-index' : 2});
-            if (childZindex == 2) $(this).css({'z-index' : 1});
-            if ($(this).hasClass('content_border')) {
-              var idName = $(this).children().first().attr('id');
-              event.stopPropagation();
-              jwplayer( idName+'_ply').pause(true);
-              $(this)
-                .removeClass('content_border content_shadow playing_small_video')
-                .css({ border : 0 })
-            }
-          })
-      } else {
-        if ($(this).hasClass('playing_small_video')) {
-          $(this).removeClass('playing_small_video')
-          var idName = $(this).children().first().attr('id');
-          event.stopPropagation();
-          jwplayer( idName+'_ply').pause(true);
-        } else { 
-          $(this).addClass('playing_small_video');
-          var idName = $(this).children().first().attr('id');
-          event.stopPropagation();
-          jwplayer( idName+'_ply').play(true);
-        }
-      }
-    }
-  })
-
-  /* embend player */
-  /** Write container **/
-  if (!document.getElementById("botr_7Wi9qfSk_GjAHfwUI_div")) {
-    document.write("<div id='botr_7Wi9qfSk_GjAHfwUI_div'></div>");
-  }
-
-  /** Insert fallback. **/
-  var videoSource = 'http://www.liandesign.hu/onLive_v3/Media/Dirt3_01.640.mp4';
-  var bgSource = 'http://content.bitsontherun.com/thumbs/7Wi9qfSk-480.jpg';
-  document.getElementById("botr_7Wi9qfSk_GjAHfwUI_div").innerHTML = "<div id='botr_7Wi9qfSk_GjAHfwUI_ply' style='background:#000 "+bgSource+"'><a href='"+videoSource+"' style='display:block; width:100%; height:100%; border:none; background:transparent url(http://content.bitsontherun.com/staticfiles/play.png) no-repeat center center; text-indent:-99999px;'>Bunny Test</a></div>";
-
-  document.getElementById("small_video_2").innerHTML = "<div id='small_video_2_ply' style='background:#000 "+bgSource+"'><a href='"+videoSource+"' style='display:block; width:100%; height:100%; border:none; background:transparent url(http://content.bitsontherun.com/staticfiles/play.png) no-repeat center center; text-indent:-99999px;'>Bunny Test</a></div>";
-  document.getElementById("small_video_3").innerHTML = "<div id='small_video_3_ply' style='background:#000 "+bgSource+"'><a href='"+videoSource+"' style='display:block; width:100%; height:100%; border:none; background:transparent url(http://content.bitsontherun.com/staticfiles/play.png) no-repeat center center; text-indent:-99999px;'>Bunny Test</a></div>";
-  document.getElementById("small_video_1").innerHTML = "<div id='small_video_1_ply' style='background:#000 "+bgSource+"'><a href='"+videoSource+"' style='display:block; width:100%; height:100%; border:none; background:transparent url(http://content.bitsontherun.com/staticfiles/play.png) no-repeat center center; text-indent:-99999px;'>Bunny Test</a></div>";
+	$('.small_video_player').on({
+		click: function(event) {
+			var clickedZindex = $(this).css('z-index');
+			if (clickedZindex != 3) {
+				var idName = $(this).children().first().attr('id');
+				event.stopPropagation();
+    		jwplayer( idName+'_ply').play();
+				$(this)
+					.addClass('content_border content_shadow playing_small_video')
+					.css({
+						'z-index' : 3,
+						'border-color': '#ffffff',
+  					'border-width' : $(this).width() * 0.03 + 'px',
+  					'border-style' : 'solid'
+					})
+					.siblings('.small_video_player').each(function() {
+						var childZindex = $(this).css('z-index');
+						if (childZindex == 3) $(this).css({'z-index' : 2});
+						if (childZindex == 2) $(this).css({'z-index' : 1});
+						if ($(this).hasClass('content_border')) {
+							var idName = $(this).children().first().attr('id');
+							event.stopPropagation();
+			    		jwplayer( idName+'_ply').pause(true);
+							$(this)
+								.removeClass('content_border content_shadow playing_small_video')
+								.css({ border : 0 })
+						}
+					})
+			} else {
+				if ($(this).hasClass('playing_small_video')) {
+					$(this).removeClass('playing_small_video')
+					var idName = $(this).children().first().attr('id');
+					event.stopPropagation();
+	    		jwplayer( idName+'_ply').pause(true);
+				} else { 
+					$(this).addClass('playing_small_video');
+					var idName = $(this).children().first().attr('id');
+					event.stopPropagation();
+	    		jwplayer( idName+'_ply').play(true);
+				}
+			}
+		}
+	});
 
 
-   /** Initialize player **/
-  jwplayer.key = "EcHWjL0bMZsdo8QE0vv5IpN4yF2kjo0m";
-  jwplayer("small_video_2_ply").setup({
+  /** Initialize players **/
+  var pl =  {playlist:
+      [{
+          image: "./images/games_site/welcome/gamepic_dirt3_headline.png",
+          sources: [
+            {file: "http://www.w3schools.com/html/movie.mp4"},
+            {file: "http://www.w3schools.com/html/movie.ogg"}
+          ]
+      }]};
+   
+  createPlayer("small_video_2_ply", pl, playerComplete);
+  createPlayer("small_video_3_ply", pl, playerComplete);
+  createPlayer("small_video_1_ply", pl, playerComplete);
+//  createPlayer("botr_7Wi9qfSk_GjAHfwUI_ply", {image: "http://content.bitsontherun.com/thumbs/7Wi9qfSk-480.jpg", playlist: "http://content.bitsontherun.com/jw6/7Wi9qfSk.xml", autostart: true, repeat: true, mute: true});
+  createPlayer("main_video_ply", $.extend({}, pl, {autostart: true, repeat: true, mute: true}));
+
+
+});
+
+
+function playerComplete(pa){
+  $('#'+pa).parents('.playing_small_video').removeClass('playing_small_video');
+}
+
+function createPlayer(pa, se, oc){
+  jwplayer(pa).setup($.extend({},{
     analytics: {"enabled": false},
     aspectratio: "16:9",
-    // strange, true value not starting the video, false however start
-    autostart: false,
-    controls: false,
-    displaytitle: false,
-    fallback: true,
-    flashplayer: "http://a.jwpcdn.com/player/6/653609/jwplayer.flash.swf",
-    height: "100%",
-    html5player: "http://a.jwpcdn.com/player/6/653609/jwplayer.html5.js",
-    image: "http://content.bitsontherun.com/thumbs/7Wi9qfSk-480.jpg",
-    playlist: "http://content.bitsontherun.com/jw6/7Wi9qfSk.xml",
-    plugins: {"http://a.jwpcdn.com/player/6/653609/ping.js": {"pixel": "http://content.bitsontherun.com/ping.gif"}},
-    primary: "flash",
-    repeat: false,
-    stretching: "uniform",
-    width: "100%"
-  });
-  //jwplayer().play();
-  //jwplayer().onComplete(function(){jwplayer().play();});
-
-  /** Initialize player **/
-  jwplayer.key = "EcHWjL0bMZsdo8QE0vv5IpN4yF2kjo0m";
-  jwplayer("small_video_3_ply").setup({
-    analytics: {"enabled": false},
-    aspectratio: "16:9",
-    // strange, true value not starting the video, false however start
-    autostart: false,
-    controls: false,
-    displaytitle: false,
-    fallback: true,
-    flashplayer: "http://a.jwpcdn.com/player/6/653609/jwplayer.flash.swf",
-    height: "100%",
-    html5player: "http://a.jwpcdn.com/player/6/653609/jwplayer.html5.js",
-    image: "http://content.bitsontherun.com/thumbs/7Wi9qfSk-480.jpg",
-    playlist: "http://content.bitsontherun.com/jw6/7Wi9qfSk.xml",
-    plugins: {"http://a.jwpcdn.com/player/6/653609/ping.js": {"pixel": "http://content.bitsontherun.com/ping.gif"}},
-    primary: "flash",
-    repeat: false,
-    stretching: "uniform",
-    width: "100%"
-  });
-  //jwplayer().play();
-  //jwplayer().onComplete(function(){jwplayer().play();});
-
-  /** Initialize player **/
-  jwplayer.key = "EcHWjL0bMZsdo8QE0vv5IpN4yF2kjo0m";
-  jwplayer("small_video_1_ply").setup({
-    analytics: {"enabled": false},
-    aspectratio: "16:9",
-    // strange, true value not starting the video, false however start
-    autostart: false,
-    controls: false,
-    displaytitle: false,
-    fallback: true,
-    flashplayer: "http://a.jwpcdn.com/player/6/653609/jwplayer.flash.swf",
-    height: "100%",
-    html5player: "http://a.jwpcdn.com/player/6/653609/jwplayer.html5.js",
-    image: "http://content.bitsontherun.com/thumbs/7Wi9qfSk-480.jpg",
-    playlist: "http://content.bitsontherun.com/jw6/7Wi9qfSk.xml",
-    plugins: {"http://a.jwpcdn.com/player/6/653609/ping.js": {"pixel": "http://content.bitsontherun.com/ping.gif"}},
-    primary: "flash",
-    repeat: false,
-    stretching: "uniform",
-    width: "100%"
-  });
-  //jwplayer().play();
-  //jwplayer().onComplete(function(){jwplayer().play();});
-
-  /** Initialize player **/
-  jwplayer.key = "EcHWjL0bMZsdo8QE0vv5IpN4yF2kjo0m";
-  jwplayer("botr_7Wi9qfSk_GjAHfwUI_ply").setup({
-    analytics: {"enabled": false},
-    aspectratio: "16:9",
-    // strange, true value not starting the video, false however start
     autostart: false,
     controls: false,
     displaytitle: false,
@@ -307,16 +242,13 @@ $(document).ready(function() {
     flashplayer: "http://a.jwpcdn.com/player/6/653609/jwplayer.flash.swf",
     height: "100%",
     html5player: "http://a.jwpcdn.com/player/6/653609/jwplayer.html5.js",
-    image: "http://content.bitsontherun.com/thumbs/7Wi9qfSk-480.jpg",
-    playlist: "http://content.bitsontherun.com/jw6/7Wi9qfSk.xml",
     plugins: {"http://a.jwpcdn.com/player/6/653609/ping.js": {"pixel": "http://content.bitsontherun.com/ping.gif"}},
-    primary: "flash",
+    primary: "html5",
     repeat: false,
     stretching: "uniform",
     width: "100%"
-  });
-  //jwplayer("").play();
-  //jwplayer().onComplete(function(){jwplayer().play();});
-
-
-});
+  },se));
+  if (oc != null){
+    jwplayer(pa).onComplete(function(){oc(pa);});
+  }
+}
